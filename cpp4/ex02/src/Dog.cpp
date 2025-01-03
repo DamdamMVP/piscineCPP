36fb42352049b83d7264692cc894a94f1d9242d0
Damien Lebranchu <dalebran@student.42lehavre.fr>
2025-01-03 12:38:00 +0100

    +: cpp4/ex02

---
+++ cpp4/ex02/src/Dog.cpp
@@ -0,0 +1,30 @@
+#include "Dog.hpp"
+
+Dog::Dog() : Animal("Dog"), brain(new Brain()) {
+  std::cout << "Dog constructor called" << std::endl;
+}
+
+Dog::Dog(const std::string &name) : Animal(name), brain(new Brain()) {
+  std::cout << "Dog constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog &other) : Animal(other), brain(new Brain(*other.brain)) {
+  std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog &Dog::operator=(const Dog &other) {
+  if (this != &other) {
+    Animal::operator=(other);
+	delete brain;
+	brain = new Brain(*other.brain);
+  }
+  std::cout << "Dog constructor called by operator" << std::endl;
+  return *this;
+}
+
+Dog::~Dog() {
+  std::cout << "Dog destructor called" << std::endl;
+  delete brain;
+}
+
+void Dog::makeSound() const { std::cout << "Woof Woof!" << std::endl; }
---
+++ cpp4/ex02/src/main.cpp
@@ -0,0 +1,10 @@
+#include "Cat.hpp"
+#include "Dog.hpp"
+
+int main() {
+  //   Animal unAnimal("dino");
+  //   std::cout << "Je suis un " << unAnimal.getType() << std::endl;
+  Dog unChien("chien");
+  std::cout << "Je suis un " << unChien.getType() << std::endl;
+  return 0;
+}
