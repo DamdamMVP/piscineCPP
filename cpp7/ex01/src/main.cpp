bba256080520b2064cbb4b131309645bc73174b2
Damien Lebranchu <dalebran@student.42lehavre.fr>
2025-01-11 18:32:48 +0100

    ~: working main

--- cpp7/ex01/src/main.cpp
+++ cpp7/ex01/src/main.cpp
@@ -3,10 +3,10 @@
 
 int main() {
   int array[5] = {1, 2, 3, 4, 5};
-  iter(array, 5, printElement);
+  iter(array, 5, printElement<int>);
 
   std::string strArray[3] = {"Hello", "World", "!"};
-  iter(strArray, 3, printElement);
+  iter(strArray, 3, printElement<std::string>);
 
   return 0;
 }
