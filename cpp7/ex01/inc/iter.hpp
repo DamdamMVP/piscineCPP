0ee35c5b06f562af35c38b06df6839ff6f5197c8
Damien Lebranchu <dalebran@student.42lehavre.fr>
2025-01-11 17:52:32 +0100

    ~: accept all types

--- cpp7/ex01/inc/iter.hpp
+++ cpp7/ex01/inc/iter.hpp
@@ -6,8 +6,8 @@ template <typename T> void printElement(T const &element) {
   std::cout << element << std::endl;
 }
 
-template <typename T>
-void iter(T *array, size_t length, void (*f)(T const &)) {
+template <typename T, typename V>
+void iter(T *array, size_t length, V f) {
   for (size_t i = 0; i < length; i++) {
     f(array[i]);
   }
